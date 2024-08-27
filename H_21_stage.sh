db589a8a9d9ef243f6782910520a4857e5480122
Frankalexej <tt115889@connect.hku.hk>
2024-08-27 14:44:42 +0800

    20240827 + 1

--- H_21_stage.sh
+++ H_21_stage.sh
@@ -17,25 +17,26 @@ pres=(0 1 2 3 4 5 10 15 20 25 30)
 ss=('c' 'v') # 
 
 # Generate a 10-digit random number
-ts=$(date +"%m%d%H%M%S")
+# ts=$(date +"%m%d%H%M%S")
 # ts="0324233831"
 # ts="0813184725"
+ts="0827104709"
 echo "Timestamp: $ts"
 # ts="0121181130"
 
 # Loop from 1 to 10, incrementing by 1
 for (( i=1; i<=5; i++ )); do
     # Loop over each combination of arguments
-    python H_21_stage.py -ts "$ts-$i" -dp
+    # python H_21_stage.py -ts "$ts-$i" -dp
     for p in "${ps[@]}"; do
-        for m in "${ms[@]}"; do
-            for s in "${ss[@]}"; do
+        for s in "${ss[@]}"; do
+            for m in "${ms[@]}"; do
                 # Randomly select a GPU between 0 and 8
                 gpu=$((RANDOM % 9))
-                post=$((55 - pre))
+                # post=$((55 - pre))
 
                 # Run the Python script with the current combination of arguments in the background
-                python H_21_stage.py -ts "$ts-$i" -p "$p" -m "$m" -s "$s" -gpu "$gpu" &
+                python H_21_stage_corun.py -ts "$ts-$i" -p "$p" -s "$s" -m "$m" -gpu "$gpu" &
             done
         done
     done
