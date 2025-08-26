06648cda47e60496b4a324a318aae27678e737d8
Frankalexej <tt115889@connect.hku.hk>
2025-08-26 15:02:02 +0800

    20250826 + 1

--- .ipynb_checkpoints/A_04_experiment_4_ae_2-checkpoint.py
+++ .ipynb_checkpoints/A_04_experiment_4_ae_2-checkpoint.py
@@ -554,7 +554,7 @@ if __name__ == "__main__":
             torch.cuda.set_device(args.gpu)
             runnumber = args.runnumber
             # model_types = ['large', 'reslin', 'lstm']
-            for preepoch in [0, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60]: # 10, 15, 20, 25, 30, 0, , 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
+            for preepoch in [1]: # 10, 15, 20, 25, 30, 0, , 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60
             # for model_type in model_types: 
                 run_once(model_save_dir, model_type=args.model, pretype=args.pretype, posttype="f", sel=args.select, 
                          preepochs=preepoch, postepochs=(120 - preepoch), save_model=False)
